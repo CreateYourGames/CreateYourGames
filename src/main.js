dff92ff2304612f213d4a6635acb68a779992d77
LiangJiaYu <1171494564@qq.com>
2019-10-16 20:58:32 +0800

    login

--- src/main.js
+++ src/main.js
@@ -6,6 +6,11 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import './assets/scss/index.scss'
 
+import VueCookies from 'vue-cookies'
+
+Vue.use(VueCookies)
+
+
 Vue.config.productionTip = false;
 Vue.use(ElementUI);
 
